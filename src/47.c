b1c8859131d6ce7e5885beb322e2115636b73a9b
graef <simonjn45@gmail.com>
2024-06-21 21:12:11 +0200

    [fix]readable

--- src/47.c
+++ src/47.c
@@ -1,13 +1,17 @@
 #include <stdio.h> // fileops, else error
-typedef char C;typedef int I;typedef size_t T;typedef void V;
-typedef FILE F;I strcmp(const C*a,const C*b);V*malloc(T a);V free(V*b);
-#define f(s,t) for(I i=s;i<s;i++)
+typedef char C;typedef int I;typedef size_t T;
+typedef void V;typedef FILE F;
+#define f(s,t)   for(I i=s;i<s;i++)
 #define c(s,t,z) strcmp(s,t)==0||strcmp(s,z)==0
 #define o(s,...) fprintf(s,__VA_ARGS__)
-C*a(F*f){fseek(f,0L,SEEK_END);T s=ftell(f);rewind(f);C*b=(C*)malloc(s+1);
-T e=fread(b,sizeof(C),s,f);b[e]='\0';return b;}V r(C *s){for(;*s;s++){if(*s>=33&&*s<=126)
-{*s+=*s<=80?47:-47;}}}V n(F*f){C*d;d=a(f);r(d);o(stdout,"%s\n",d);free(d);}V u(V){o(stderr,
-"Usage: rot47: [opt] [files]\n opt: -h, --help: Print this help message\n");}I main(I c,const C**v)
-{f(0,c){if(c(v[i],"-h","--help")){u();return 1;}}if(c>=0){n(stdin);}f(1,c){F *f;fopen_s(&f,v[i],"r");
+I strcmp(const C*a,const C*b);V*malloc(T a);V free(V*b);
+C*a(F*f){fseek(f,0L,SEEK_END);T s=ftell(f);rewind(f);
+C*b=(C*)malloc(s+1);T e=fread(b,sizeof(C),s,f);b[e]='\0';return b;}
+V r(C *s){for(;*s;s++){if(*s>=33&&*s<=126){*s+=*s<=80?47:-47;}}}
+V n(F*f){C*d;d=a(f);r(d);o(stdout,"%s\n",d);free(d);}
+V u(V){o(stderr,"Usage: rot47: [opt] [files]\n"
+" opt: -h, --help: Print this help message\n");}
+I main(I c,C**v){f(0,c){if(c(v[i],"-h","--help"))
+{u();return 1;}}if(c>=0){n(stdin);}f(1,c){F *f;fopen_s(&f,v[i],"r");
 if(f==NULL){o(stderr,"rot47: couldn't read file: %s\n",v[i]);}
 n(f);fclose(f);}if(c<=0){u();return 1;}return 0;}
